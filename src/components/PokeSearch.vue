b3d47f4013abc04824280cae4b2b967143bbcd35
Florian HUGOT <flo.hugot@gmail.com>
2020-07-21 16:08:26 +0200

    Correction barre de recherche avec caractères en majuscules

--- src/components/PokeSearch.vue
+++ src/components/PokeSearch.vue
@@ -9,6 +9,7 @@
                 class="form-control" 
                 placeholder="Find a Pokemon" 
                 v-model="searchvalue"
+                @input="searchvalue = $event.target.value.toLowerCase()"
             >
         </form>
     </div>
@@ -24,10 +25,16 @@ export default {
             searchvalue: ''
         }
     },
+    computed: {
+        searchvalueToLowerCase() {
+            return this.searchvalue.toLowerCase().trim()
+        }
+    },
     methods: {
         setPokemonUrl() {
             if(this.searchvalue !== '') {
                 this.$emit('setPokemonUrl', this.apiUrl + this.searchvalue)
+                // this.searchvalue.toLowerCase()
             }
         }
     }
